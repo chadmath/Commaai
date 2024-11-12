50678c9dd9326699ee9bb74909e1802768dcbaca
chadmath <chadmathiason@hotmail.com>
2024-11-12 13:37:19 -0600

    Update v079.sh

--- v079.sh
+++ v079.sh
@@ -3,11 +3,11 @@
 set -e
 
 DEFAULT_GITHUB_OWNER="commaai"
-DEFAULT_GITHUB_BRANCH="tags/v0.7.9"
+DEFAULT_GITHUB_TAG="v0.7.9"
 
 cd /data
 rm -rf openpilot
-time git clone https://github.com/${1:-$DEFAULT_GITHUB_OWNER}/openpilot.git openpilot -b ${2:-$DEFAULT_GITHUB_BRANCH} --recurse-submodules --depth 1
+time git clone https://github.com/${1:-$DEFAULT_GITHUB_OWNER}/openpilot.git openpilot --branch ${2:-$DEFAULT_GITHUB_TAG} --recurse-submodules --depth 1
 
 cd /data/data/com.termux/files
 echo $'#!/usr/bin/bash\n\ncd /data/openpilot\n./launch_openpilot.sh\n' > continue.sh
